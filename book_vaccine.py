d2d86d53fcc66c117a3ae07f6133b68b0b0cfb67
Jophine Antony <jophinep@gmail.com>
2021-08-18 14:21:29 +0530

    Updated script

--- book_vaccine.py
+++ book_vaccine.py
@@ -7,6 +7,7 @@ from time import sleep
 today = date.today().strftime(r"%d-%m-%Y")
 beneficiaries = [
 ]
+my_loc = (8, 76)
 
 # Urls
 generate_otp_url = "https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP"
@@ -34,7 +35,6 @@ validate_otp_data = json.dumps({
 votp_resp = requests.post(validate_otp_url, data=validate_otp_data)
 token = votp_resp.json()["token"]
 
-print(token)
 auth_header = {
     "authorization": f"Bearer {token}"
 }
@@ -45,16 +45,21 @@ def book_now():
 
     for i in data["centers"]:
         if i["fee_type"] == "Free" and i["block_name"] == "Thiruvananthapuram":
+            # distance = sqrt((x1-x2)^2 + (y1-y2)^2)
+            dist = ((my_loc[0] - i["lat"]) ** 2 + (my_loc[1] - i["long"]) ** 2) ** 0.5 * 111.321
             for j in i["sessions"]:
-                if j["vaccine"] == "COVISHIELD" and (j["min_age_limit"] == 18 or j["allow_all_age"] is True) and j["available_capacity_dose2"] != 0:
-                    print(i["name"])
-                    print(i["address"])
-                    print(j["slots"][0])
-                    print(j["available_capacity_dose2"])
+                if (j["vaccine"] == "COVISHIELD" and
+                        (j["min_age_limit"] == 18 or j["allow_all_age"] is True) and
+                        j["available_capacity_dose2"] != 0):
+                    print("Name: ", i["name"])
+                    print("Address: ", i["address"])
+                    print("Distance: ", dist)
+                    print("Slots: ", j["slots"][0])
+                    print("Availability: ", j["available_capacity_dose2"])
                     schedule_data = json.dumps({
                         "dose": 2,
                         "session_id": j["session_id"],
-                        "slot": j["slot"][0],
+                        "slot": j["slots"][0],
                         "beneficiaries": beneficiaries
                     })
                     schedule_resp = requests.post(schedule_url, data=schedule_data)
@@ -67,20 +72,17 @@ def book_now():
 
 book_now()
 
-# logout
-# keep_alive = True
-# while (keep_alive):
 try:
     for _i in range(0, 180):
         print(f"Try time: {datetime.now()}")
         book_now()
         print("No slot found! Will try in 30 seconds")
         sleep(30)
-except KeyboardInterrupt:
-    pass
+except (KeyboardInterrupt, KeyError, IndexError) as err:
+    print(err)
 finally:
     print("Exiting now")
 
-# keep_alive = input("Should I logout? ")
+# logout
 logout_resp = requests.get(logout_url, headers=auth_header)
 print(logout_resp.status_code)
